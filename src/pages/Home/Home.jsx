5585da8c535ef278250afb7600c61887bef07676
Dikshit Suthar <suthardikshit57@gmail.com>
2024-05-27 16:16:01 +0530

    minor changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import Hero from "../../components/Hero/Hero";
 import '../../styles/home.css'
 import Footer from "../../components/Footer/Footer";
 // import 'bootstrap/dist/css/bootstrap.min.css'
