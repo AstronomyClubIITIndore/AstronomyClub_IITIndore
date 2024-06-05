a916211c8bc1339f26263384d9cafee4c4e2ff57
Abhishek A P <abhi2004iiti@gmail.com>
2024-06-05 13:58:21 +0530

    projects page

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
-import '../../styles/projects.css'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import "../../styles/projects.css";
+import PCard from "./ProjectsCard";
 import Footer from "../../components/Footer/Footer";
 
 function Projects() {
   return (
     <>
-    
       <div className="Projects-encloser1">
-      <Navbar/>
-      <div className="Projects-encloser2"> <div className="Projects-heading">Projects and Innovations</div></div>
-       
+        <Navbar />
+        <div className="Projects-encloser2">
+          {" "}
+          <div className="Projects-heading">Projects and Innovations</div>
+        </div>
       </div>
-      <Footer/>
+      <PCard></PCard>
+      <PCard></PCard>
+      <PCard></PCard>
+      <PCard></PCard>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
---
+++ src/pages/Projects/ProjectsCard.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "../../styles/projects.css";
+
+function PCard() {
+  return (
+    <>
+      <div className="Projects-Card">
+        <div className="Photo"></div>
+        <div className="Info">
+          <h2>Project Name</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum
+            explicabo odio, sit sapiente maiores earum placeat ex tempora natus.
+            Molestiae iusto consectetur voluptatibus ex modi facere corporis
+            accusantium voluptas dicta, id vel suscipit hic qui omnis officia.
+            Est nihil libero accusantium consectetur temporibus vel porro saepe
+            neque! Optio voluptatum ut inventore vero obcaecati, dolorem
+            laudantium atque, eveniet quidem, saepe corrupti? Consequatur fugiat
+            pariatur totam, nesciunt in impedit odit quod cupiditate cumque hic
+            delectus explicabo et labore! Quibusdam quod voluptate optio tenetur
+            expedita modi, in ipsum dolor odit, et temporibus facilis maxime
+            dolores quas deleniti dolore dolorum reiciendis accusamus, ex
+            sapiente nisi veniam. Earum, odio modi animi molestiae, eius quo
+            quibusdam, temporibus cupiditate facere obcaecati corrupti amet
+            expedita numquam accusamus repellat.
+          </p>
+          <div>
+            <button className="button">Link</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default PCard;
