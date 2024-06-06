1813e7372ee927a588f478862369bd45b3792ca5
Dikshit Suthar <suthardikshit57@gmail.com>
2024-06-06 13:39:23 +0530

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,29 @@ import Pages from './pages/pages'
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { useEffect, useState } from 'react';
+import Loader from './components/loader';
 
 
 function App() {
+     const[loading,setLoading]=useState(false);
+
+     useEffect(
+    ()=>{
+      setLoading(true);
+      setTimeout(()=>{
+        setLoading(false);
+      },2000);  },[]
+     )
+  
+  if(loading){
+    return <Loader/>}
+else{
   return (
     <>
   <Pages/>
-    </>
+      </>
   )
 }
-
+}
 export default App
---
+++ src/components/loader.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import '../styles/loader.css'
+function Loader() {
+  return (
+   <>
+   <div className="flex items-center justify-center h-[100vh]"><div class="section-banner">
+   <div id="star-1">
+     <div class="curved-corner-star">
+       <div id="curved-corner-bottomright"></div>
+       <div id="curved-corner-bottomleft"></div>
+     </div>
+     <div class="curved-corner-star">
+       <div id="curved-corner-topright"></div>
+       <div id="curved-corner-topleft"></div>
+     </div>
+   </div>
+ 
+   <div id="star-2">
+     <div class="curved-corner-star">
+       <div id="curved-corner-bottomright"></div>
+       <div id="curved-corner-bottomleft"></div>
+     </div>
+     <div class="curved-corner-star">
+       <div id="curved-corner-topright"></div>
+       <div id="curved-corner-topleft"></div>
+     </div>
+   </div>
+ 
+   <div id="star-3">
+     <div class="curved-corner-star">
+       <div id="curved-corner-bottomright"></div>
+       <div id="curved-corner-bottomleft"></div>
+     </div>
+     <div class="curved-corner-star">
+       <div id="curved-corner-topright"></div>
+       <div id="curved-corner-topleft"></div>
+     </div>
+   </div>
+ 
+   <div id="star-4">
+     <div class="curved-corner-star">
+       <div id="curved-corner-bottomright"></div>
+       <div id="curved-corner-bottomleft"></div>
+     </div>
+     <div class="curved-corner-star">
+       <div id="curved-corner-topright"></div>
+       <div id="curved-corner-topleft"></div>
+     </div>
+   </div>
+ 
+   <div id="star-5">
+     <div class="curved-corner-star">
+       <div id="curved-corner-bottomright"></div>
+       <div id="curved-corner-bottomleft"></div>
+     </div>
+     <div class="curved-corner-star">
+       <div id="curved-corner-topright"></div>
+       <div id="curved-corner-topleft"></div>
+     </div>
+   </div>
+ 
+   <div id="star-6">
+     <div class="curved-corner-star">
+       <div id="curved-corner-bottomright"></div>
+       <div id="curved-corner-bottomleft"></div>
+     </div>
+     <div class="curved-corner-star">
+       <div id="curved-corner-topright"></div>
+       <div id="curved-corner-topleft"></div>
+     </div>
+   </div>
+ 
+   <div id="star-7">
+     <div class="curved-corner-star">
+       <div id="curved-corner-bottomright"></div>
+       <div id="curved-corner-bottomleft"></div>
+     </div>
+     <div class="curved-corner-star">
+       <div id="curved-corner-topright"></div>
+       <div id="curved-corner-topleft"></div>
+     </div>
+   </div>
+ </div>
+ <div class="loader pl-6">
+   <div data-glitch="Loading..." class="glitch">Loading...</div>
+</div>
+ </div>
+ 
+   </>
+  )
+}
+
+export default Loader
