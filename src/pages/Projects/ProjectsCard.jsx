7434cff4241c9fde173d625d53a7964417e86fdf
Abhishek A P <abhi2004iiti@gmail.com>
2024-06-07 15:42:18 +0530

    minor changes in projects page

--- src/pages/Projects/ProjectsCard.jsx
+++ src/pages/Projects/ProjectsCard.jsx
@@ -1,31 +1,22 @@
 import React from "react";
 import "../../styles/projects.css";
 
-function PCard() {
+function PCard(project) {
   return (
     <>
       <div className="Projects-Card">
-        <div className="Photo"></div>
+        <div
+          className="Photo"
+          style={{
+            backgroundImage: `url(${project.img})`,
+          }}></div>
         <div className="Info">
-          <h2>Project Name</h2>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum
-            explicabo odio, sit sapiente maiores earum placeat ex tempora natus.
-            Molestiae iusto consectetur voluptatibus ex modi facere corporis
-            accusantium voluptas dicta, id vel suscipit hic qui omnis officia.
-            Est nihil libero accusantium consectetur temporibus vel porro saepe
-            neque! Optio voluptatum ut inventore vero obcaecati, dolorem
-            laudantium atque, eveniet quidem, saepe corrupti? Consequatur fugiat
-            pariatur totam, nesciunt in impedit odit quod cupiditate cumque hic
-            delectus explicabo et labore! Quibusdam quod voluptate optio tenetur
-            expedita modi, in ipsum dolor odit, et temporibus facilis maxime
-            dolores quas deleniti dolore dolorum reiciendis accusamus, ex
-            sapiente nisi veniam. Earum, odio modi animi molestiae, eius quo
-            quibusdam, temporibus cupiditate facere obcaecati corrupti amet
-            expedita numquam accusamus repellat.
-          </p>
+          <h2>{project.pName}</h2>
+          <p>{project.pInfo}</p>
           <div>
-            <button className="button">Link</button>
+            <a href={project.pLink} className="button">
+              Link
+            </a>
           </div>
         </div>
       </div>
