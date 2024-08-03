ac2b126abd92a52475c02e6ad8cff164a51a1339
Vandan Nagori <vandannagori@Vandans-MacBook-Air.local>
2024-08-03 23:17:05 +0530

    Updated Activites and Projects

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -7,42 +7,34 @@ import Footer from "../../components/Footer/Footer";
 function Projects() {
   const projectsInfo = [
     {
-      pName: "Stargazing",
-      img: "https://i0.wp.com/wallpapercave.com/wp/wp3493599.jpg?ssl=1",
+      pName: "Remote Weather Station with drone adaptability",
+      img: "https://www.mdpi.com/drones/drones-07-00340/article_deploy/html/images/drones-07-00340-g001.png",
       pInfo:
-        "Stargazing is a primary event organized monthly by the Astronomy Club at IIT Indore. Supported by the Department of Astronomy, Astrophysics, and Space Engineering (DAASE), the club uses various telescopes to observe celestial objects visible in the night sky. These events offer participants an opportunity to explore planets, stars, and galaxies, often guided by experts. The sessions foster curiosity and a deeper understanding of the cosmos, attracting enthusiasts from across the IIT Indore community.",
+        "The Remote Weather Station project focuses on developing payloads for drones that can measure temperature, pressure, humidity, and gas composition. The payloads will be integrated with communication systems to transmit collected data in real-time. The project involves designing and testing these payloads to ensure accurate and reliable data transmission. The goal is to enhance weather monitoring capabilities and provide valuable information for various applications, including environmental studies and disaster management.",
       pLink:
         "https://github.com/AstronomyClubIITIndore/AstronomyClub_IITIndore/tree/main",
     },
     {
-      pName: "Guest Lectures",
-      img: "https://drive.google.com/uc?export=view&id=1BQcDeQ4ytZRYZyMPzlfS4arBxM_JYcix",
+      pName: "Ion Plasma Thruster",
+      img: "https://i.ytimg.com/vi/mnCmvxt2jn8/maxresdefault.jpg",
       pInfo:
-        "Guest Lectures are a significant feature of the Astronomy Club at IIT Indore. The club regularly invites speakers from around the world, including renowned researchers, industry experts, and innovative thinkers. These guest lectures provide a platform for speakers to share their latest research findings, offer insights into industry trends, discuss future prospects in the field, and present their innovative ideas. Additionally, speakers often share their personal experiences and career journeys, providing valuable inspiration and guidance to students. These sessions not only enhance the knowledge of club members but also foster a deeper understanding of the vast and dynamic field of astronomy and space science.",
+        "The development of a functional Ionic Plasma Thruster prototype involves designing and constructing a device that generates thrust through the acceleration of ionized gases. This project includes creating a robust ionization chamber, optimizing the ion acceleration process, and testing the thruster's performance in various conditions. The prototype aims to demonstrate efficient propulsion and precise control, contributing to advancements in space exploration technology. Key challenges include achieving high thrust-to-power ratios and ensuring operational stability in a vacuum environment.",
       pLink:
         "https://github.com/AstronomyClubIITIndore/AstronomyClub_IITIndore/tree/main",
     },
     {
-      pName: "Kendriya Vidyalaya activities",
-      img: "https://drive.google.com/uc?export=view&id=1vW445yW3YgGL8h96E7esyWU4oQKMt3eo",
+      pName: "Magneto-torquer for ADCS system",
+      img: "https://www.ece3sat.com/images/ADCS_subsystem.png",
       pInfo:
-        "As part of its outreach initiatives, the Astronomy Club at IIT Indore organizes several educational activities at Kendriya Vidyalaya, IIT Indore. These activities include Sun-Spot viewing sessions, where students get to observe solar phenomena using special equipment. Additionally, the club conducts creative sessions such as glass painting and 3D model making, which help students visualize and understand astronomical concepts. The outreach also features talk sessions delivered by club members in collaboration with DAASE faculties, providing students with insights into astronomy and space science. These activities aim to ignite a passion for science and astronomy among young students.",
+        "The Magneto-Torquer for ADCS system project involves developing an Attitude Determination and Control System (ADCS) for a CubeSat, integrating magneto-torquers, reaction wheels, and gyroscopes. The magneto-torquers use Earth's magnetic field to control the satellite's orientation, while reaction wheels provide precise angular momentum control. Gyroscopes measure the satellite's rotational rates to support accurate attitude determination. The project focuses on optimizing these components to ensure stable and reliable satellite positioning and control for various space missions.",
       pLink:
         "https://github.com/AstronomyClubIITIndore/AstronomyClub_IITIndore/tree/main",
     },
     {
-      pName: "Nakshatra",
-      img: "https://i0.wp.com/wallpapercave.com/wp/wp3493599.jpg?ssl=1",
+      pName: "Data processing and analysis",
+      img: "https://img.freepik.com/premium-vector/digital-concept-big-data-is-fluttering-data-collection-data-processing-data-analysis-beautiful-futuristic-background_36402-1420.jpg",
       pInfo:
-        "Nakshatra is the annual flagship event of the AsSET club at IIT Indore, celebrating the wonders of astronomy and space science. Last year, the club represented the '100 Hours of Astronomy', an international outreach initiative by the International Astronomical Union (IAU) aimed at engaging people in exploring and appreciating the marvels of the astronomical world. During Nakshatra, the AsSET club organized public stargazing sessions, interactive workshops, and lectures by renowned astronomers. These activities foster a deeper understanding of the cosmos and inspire a sense of wonder about the universe among participants. Through Nakshatra, the AsSET club continues to promote the importance of space science and technology.",
-      pLink:
-        "https://github.com/AstronomyClubIITIndore/AstronomyClub_IITIndore/tree/main",
-    },
-    {
-      pName: "World Space Week",
-      img: "https://i0.wp.com/wallpapercave.com/wp/wp3493599.jpg?ssl=1",
-      pInfo:
-        "World Space Week is an annual global event celebrated in 95 countries, recognizing the contributions of science and technology to improving the human condition. The AsSET club at IIT Indore actively participates in this event, contributing to its activities and initiatives. The club organizes various events and educational programs, such as public lectures, interactive workshops, and stargazing sessions, to engage the community and raise awareness about the importance of space science and technology. Through these efforts, the AsSET club aims to inspire and educate participants about the wonders and potential of space exploration.",
+        "The Data Processing and Analysis project involves creating programs and algorithms to handle and interpret data from diverse sources like payloads, observatories, telescopes, radars, and receivers. This includes developing efficient methods for data integration, cleaning, and transformation to ensure accuracy and consistency. The goal is to extract meaningful insights and generate actionable information from complex datasets. The project also focuses on optimizing processing speeds and ensuring scalability to handle large volumes of data effectively.",
       pLink:
         "https://github.com/AstronomyClubIITIndore/AstronomyClub_IITIndore/tree/main",
     },
